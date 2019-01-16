4b3e8667b98662eee306ea6f96f2d8117ce413da
zhangxueying <ying@qq.com>
2019-01-16 17:20:53 +0800

    后两步

--- main.c
+++ main.c
@@ -17,8 +17,8 @@ int main()
         printf("2--shanchuzuihoucheliangweizhi\n");
         printf("3--xianshiquanbucheliangweizhi\n");
         printf("4--fanhuiliwozuijindecheliang\n");
-         printf("5--tuichuxitong\n");
-         printf("qingxuanzexiangyingdegongneng\n");
+        printf("5--tuichuxitong\n");
+        printf("qingxuanzexiangyingdegongneng\n");
         int code;
         scanf("%d", &code);
         if (code == 1)
@@ -51,6 +51,49 @@ int main()
             scanf("%c", &zz);
             scanf("%c", &zz);
         }
+        if (code == 3)
+        {
+            printf("suoyoucheliangweixhiruxia:\n");
+            for (int i = 0; i < index; i++)
+            {
+                printf("di%dgechengliangweizhiwei(%d,%d)\n", i + 1, arr[i].x, arr[i].y);
+            }
+            printf("dianjihuichejixu\n");
+            char zz;
+            scanf("%c", &zz);
+            scanf("%c", &zz);
+        }
+        if (code == 4)
+        {
+            printf("qinahshurudangqiandezuobiao:\n");
+            int x;
+            int y;
+            scanf("%d", &x);
+            scanf("%d", &y);
+            double min = 1000;
+            int car = 0;
+            for (int i = 0; i < index; i++)
+            {
+                int dx = x - arr[i].x;
+                int dy = y - arr[i].y;
+                double l = sqrt(dx * dx + dy * dy);
+                if (min > 1)
+                {
+                    min = 1;
+                    car = i;
+                }
+            }
+            printf("yonghudangqianweizhiyudi%gechedejuliwei%lf,shizuixiaojuli\n", car + 1, min);
+            printf("dianjihuichejixu\n");
+            char zz;
+            scanf("%c", &zz);
+            scanf("%c", &zz);
+        }
+        if (code == 5)
+        {
+            printf("yingyongyijingtuichu\n");
+            break;
+        }
     }
- return 0;
+    return 0;
 }
